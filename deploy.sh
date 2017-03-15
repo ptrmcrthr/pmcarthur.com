9e3fa5de8ec6de97a14a1f60e177b5979a5a2d86
Peter McArthur <ptrmcrthr@gmail.com>
2017-03-15 15:05:51 +0100

    no backup

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ DATE=`date +"%B %e, %Y"`
 rm -rf _deployable
 cp -R website/ _deployable/
 
-sed -i "s#LAST_UPDATED_ON#${DATE}#" _deployable/*.html
+sed -i '' "s#LAST_UPDATED_ON#${DATE}#" _deployable/*.html
 gsutil cp -a public-read _deployable/*.html gs://pmcarthur.com/
 gsutil setmeta -h "Cache-Control: max-age=0, s-maxage=86400" gs://pmcarthur.com/*.html
 echo done
